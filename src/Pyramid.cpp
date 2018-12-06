6146d9f0eccb4b3bf2611a55036a19d2a47d0bfa
Theerawach Chockruamchai <eeytc3@nottingham.ac.uk>
2018-12-06 21:56:28 +0000

    Finished Pyramid

--- src/Pyramid.cpp
+++ src/Pyramid.cpp
@@ -2,19 +2,26 @@
 // Created by matt on 08/11/18.
 //
 
+#include <d3dvec.inl>
 #include "Pyramid.h"
 
 float Pyramid::calcVolume() const {
-    const std::vector<Vec3> vertices = Cell::getCellVertices();
+    std::vector<Vec3> vertices = Cell::getCellVertices();
     float volume;
     // TODO: Write function which will return volume of pyramid from array of vertices
+    Vec3 A = vertices[0] - vertices[1]; //VectA
+    Vec3 B = vertices[0] - vertices[3]; //VectB
+    Vec3 C = vertices[0] - vertices[4]; //VectC
+    volume = (A*B).dot(C) * (1/3);
     return volume;
 }
 
 Vec3 Pyramid::calcCentreofGravity() const {
-    const std::vector<Vec3> vertices = Cell::getCellVertices();
+    std::vector<Vec3> vertices = Cell::getCellVertices();
     Vec3 cog;
     // TODO: Write function which will return centre of gravity of pyramid from array of vertices
+    Vec3 Sum = vertices[0]+ vertices[1]+ vertices[2]+vertices[3]+vertices[4];
+    cog = Sum/5;
     return cog;
 }
 
@@ -23,5 +30,8 @@ float Pyramid::calcWeight() const {
     const float volume = Cell::getVolume();
     float weight = 0;
     // TODO: Write function which will return weight of pyramid from volume and material
+    material.getDensity();
+    this->getVolume();
+    weight = Density*volume;
     return weight;
 }
